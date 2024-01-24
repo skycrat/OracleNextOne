f69f65101ccd9b80f43f3f1f4ed7f21f74ac781b
Katia Celestino <59209617+skycrat@users.noreply.github.com>
2024-01-24 03:30:48 +0000

    adding text

--- Reiniciando el juego/app.js
+++ Reiniciando el juego/app.js
@@ -15,3 +15,6 @@ function generarNumeroSecreto()
 {
     return Math.floor(Math.random() * 10)+1;
 }
+
+asignarTextoElemento('h1', 'Juego del número secreto!');
+asignarTextoElemento('p', `Indica un número del 1 al 10`);
