194ef969a0b7d82d5a0f7d4bfc40859709ee065d
Katia Celestino <59209617+skycrat@users.noreply.github.com>
2024-01-24 07:07:52 +0000

    putting in function everything and adding reiniciar juego function

--- Reiniciando el juego/app.js
+++ Reiniciando el juego/app.js
@@ -12,7 +12,7 @@ function verificarIntento(){
     
     if (numeroSecreto === numeroDeUsuario) {
         asignarTextoElemento('p', `Acertaste el número en ${intentos} ${intentos == 1 ? 'vez':'veces'}`);
-        document.getElementById('reiniciar').removeAttribute('disabled');
+        document.getElementById("reiniciar").removeAttribute('disabled');
 
     }  else {
         //El usuario no acertó
@@ -42,9 +42,25 @@ function limpiarCaja(){
     document.querySelector('#valorUsuario').value = '';
 }
 
+function condicionesIniciales(){
+    asignarTextoElemento('h1', 'Juego del número secreto!');
+    asignarTextoElemento('p', `Indica un número del 1 al 10`);
+    numeroSecreto = generarNumeroSecreto();
+    intentos = 1;
+}
+
+function reiniciarJuego(){
+    //limpiar caja
+    limpiarCaja();
+    //indicar mensaje
+    //Generar el numero aleatorio
+    //Inicializar el numero de intentos
+    condicionesIniciales();
+    //deshabilitar boton de nuevo juego
+}
+
 function generarNumeroSecreto(){
     return Math.floor(Math.random() * 10)+1;
 }
 
-asignarTextoElemento('h1', 'Juego del número secreto!');
-asignarTextoElemento('p', `Indica un número del 1 al 10`);
+mensajesIniciales()
