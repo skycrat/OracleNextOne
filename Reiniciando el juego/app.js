1a61bc8b6194e331a86d9969f825ed16b516848b
Katia Celestino <59209617+skycrat@users.noreply.github.com>
2024-01-24 03:22:52 +0000

    adding verificar intento y generar numero secreto

--- Reiniciando el juego/app.js
+++ Reiniciando el juego/app.js
@@ -1,1 +1,17 @@
-//para editar
+let numeroSecreto = generarNumeroSecreto();
+
+function asignarTextoElemento(elemento, texto){
+    let elementoHTML = document.querySelector(elemento);
+    elementoHTML.innerHTML = texto
+    return;
+}
+
+function verificarIntento(){
+    let numeroDeUsuario = parseInt(document.getElementById('valorUsuario').value)
+    return;
+}
+
+function generarNumeroSecreto()
+{
+    return Math.floor(Math.random() * 10)+1;
+}
