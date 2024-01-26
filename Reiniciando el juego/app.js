7fa675880015c05c4b88a155317d891bd42f7d27
Katia Celestino <59209617+skycrat@users.noreply.github.com>
2024-01-26 04:49:45 +0000

    aplicando recursividad a la funcion generarNumeroSecreto

--- Reiniciando el juego/app.js
+++ Reiniciando el juego/app.js
@@ -1,5 +1,6 @@
 let numeroSecreto = generarNumeroSecreto();
 let intentos = 1
+let listNumerosSorteados = []
 
 function asignarTextoElemento(elemento, texto){
     let elementoHTML = document.querySelector(elemento);
@@ -61,7 +62,15 @@ function reiniciarJuego(){
 }
 
 function generarNumeroSecreto(){
-    return Math.floor(Math.random() * 10)+1;
+    let numeroGenerado = Math.floor(Math.random() * 10)+1;
+
+    //Si el numero generado esta incluido en la lista
+    if (listNumerosSorteados.includes(numeroGenerado)){
+            return generarNumeroSecreto();
+    } else {
+        listNumerosSorteados.push(numeroGenerado);
+        return numeroGenerado;
+    }
 }
 
 condicionesIniciales()
